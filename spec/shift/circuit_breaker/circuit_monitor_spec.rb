727c15b922ffebcbe4c9f3e54190cb104829600e
Mufudzi Masaire <mufudzi.masaire@shiftcommerce.com>
2018-02-05 11:40:06 +0000

    Update specs for Shift::CircuitBreaker::CircuitMonitor

--- spec/shift/circuit_breaker/circuit_monitor_spec.rb
+++ spec/shift/circuit_breaker/circuit_monitor_spec.rb
@@ -4,18 +4,39 @@ module Shift
   module CircuitBreaker
     describe CircuitMonitor do
 
-      context "Recording metrics" do
+      context "#record_metric" do
         it "logs the metric information" do
           # Arrange
-          logger_instance = ::Logger.new(STDOUT)
-          monitor = described_class.new(logger: logger_instance)
-          circuit_breaker_name = :test_circuit_breaker
+          circuit_breaker_name  = :test_circuit_breaker
           circuit_breaker_state = :open
-          metric = "Custom/#{circuit_breaker_name.to_s.classify}CircuitBreaker/#{circuit_breaker_state.to_s.classify}"
+          logger_instance       = ::Logger.new(STDOUT)
+          monitor               = described_class.new(logger: logger_instance)
+          metric                = "Custom/#{circuit_breaker_name.to_s.classify}CircuitBreaker/#{circuit_breaker_state.to_s.classify}"
 
-          # Act & Assert
-          expect(logger_instance).to receive(:info).with(include(metric))
+          allow(logger_instance).to receive(:info)
+
+          # Act 
           monitor.record_metric(circuit_breaker_name, circuit_breaker_state)
+
+          # Assert
+          expect(logger_instance).to have_received(:info).with(include(metric))
+        end
+
+        it "logs the metric information using the provided monitor" do
+          # Arrange
+          circuit_breaker_name  = :test_circuit_breaker
+          circuit_breaker_state = :open
+          metric_monitor        = Shift::CircuitBreaker::Adapters::NewRelicAdapter
+          monitor               = described_class.new(monitor: metric_monitor)
+          metric                = "Custom/#{circuit_breaker_name.to_s.classify}CircuitBreaker/#{circuit_breaker_state.to_s.classify}"
+
+          allow(metric_monitor).to receive(:call)
+
+          # Act 
+          monitor.record_metric(circuit_breaker_name, circuit_breaker_state)
+
+          # Assert
+          expect(metric_monitor).to have_received(:call).with(metric)
         end
       end 
 
